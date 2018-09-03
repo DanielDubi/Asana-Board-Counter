d544d39e5e1d8bba25d074a2f1131a4cb8e08f27
Daniel Duvilanski <daniel.d@qspark.co>
2018-09-03 11:20:07 +0300

    Added disaply as table instead of less pretty <br>
    Also added a button that will check stuff in the board

--- asana_counter.js
+++ asana_counter.js
@@ -8,6 +8,55 @@
 // @grant        none
 // ==/UserScript==
 
+var checkButtonAdded = false;
+
+function checkLaunchpad(title, column) {
+    return "";
+}
+
+function checkColumn(title, column) {
+    var currentItems = column.childNodes.length;
+    var titile = title.children[1].innerHTML;
+    var max = titile.match(/\d+/g);
+    if (max === null)
+    {
+        max = 0;
+    }
+    console.log(titile + " max=" + max + " current=" + currentItems);
+    if (max === 0)
+    {
+        return "";
+    }
+    if (currentItems > max)
+    {
+        return titile + " is overflowing\n";
+    }
+    return "";
+}
+
+function checkBoard() {
+    var result = "";
+    var titles = document.getElementsByClassName("BoardColumnHeader BoardColumn-header");
+    var columns = document.getElementsByClassName("SortableList-itemContainer SortableList-itemContainer--column");
+    if (columns[0].childNodes.length > 0)
+    {
+        result += titles[0].children[1].innerHTML + " is overflowing\n";
+    }
+    for (var i = 1; i < columns.length; i++)
+    {
+        result += checkColumn(titles[i], columns[i]);
+    }
+    result += checkLaunchpad(titles[1], columns[1]);
+    if (result === "")
+    {
+        window.confirm("Board is good!");
+    }
+    else
+    {
+        window.confirm(result);
+    }
+}
+
 function addCount(element, number) {
     var span = document.createElement("span");
     var spanText = document.createTextNode(" (" + number + ") ");
@@ -23,6 +72,21 @@ function addCount(element, number) {
     }
 }
 
+function addCheckButton() {
+    'use strict';
+    if (checkButtonAdded)
+    {
+        return;
+    }
+    var boardHeader = document.getElementsByClassName("BoardHeader Board-header");
+    var btn = document.createElement("BUTTON");
+    var text = document.createTextNode("Check Board");
+    btn.appendChild(text);
+    btn.onclick = function() { checkBoard(); }
+    boardHeader[0].appendChild(btn);
+    checkButtonAdded = true;
+}
+
 setInterval(function() {
     'use strict';
     var titles = document.getElementsByClassName("BoardColumnHeader BoardColumn-header");
@@ -30,7 +94,7 @@ setInterval(function() {
     for (var i = 0; i < columns.length; i++)
     {
         var childs = columns[i].childNodes;
-        var titleChild = titles.childNodes;
         addCount(titles[i], childs.length);
     }
+    addCheckButton();
 }, 5000);
